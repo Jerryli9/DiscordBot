70dc402a05fbb61b1f1b8cc0ef4a5ce68003e70c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-16 10:49:22 +1000

    initialize github repository

---
+++ main.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log('Bot has logged in')
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content== "hi"){
+        message.reply("Hello! How may I help you?")
+    }
+})
+
+client.login(process.env.TOKEN)
